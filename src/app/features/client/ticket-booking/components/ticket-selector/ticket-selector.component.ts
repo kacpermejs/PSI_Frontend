3d48f94cb9a3d2534bbb7bdc37a5ee8138ddfcde
Kacper Mejsner <kacpermejs@gmail.com>
2025-02-03 14:26:38 +0100

    order in to payment

--- src/app/features/client/ticket-booking/components/ticket-selector/ticket-selector.component.ts
+++ src/app/features/client/ticket-booking/components/ticket-selector/ticket-selector.component.ts
@@ -6,6 +6,9 @@ import { TicketStoreService } from '../../services/ticket-store/ticket-store.ser
 import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
+import { OrderService } from 'app/features/client/client-cart/services/order.service';
+import { CognitoService } from '@core/services/cognito/cognito.service';
+import { OrderTicketDTO } from '@core/models/order/OrderTicketDTO';
 
 @Component({
   selector: 'app-ticket-selector',
@@ -21,11 +24,52 @@ export class TicketSelectorComponent implements OnInit {
   orderStoreService = inject(TicketStoreService);
   tickets$?: Observable<Map<number, OrderData>>;
 
+  orderService = inject(OrderService);
+  ticketStore = inject(TicketStoreService);
+  cognito = inject(CognitoService);
+
   ngOnInit(): void {
     this.tickets$ = this.orderStoreService.selectedSeats$;
   }
 
   navigateToCheckout() {
-    this.router.navigate(['/cart']);
+    const data = this.getOrderData();
+
+    this.cognito.getUserData().then(user => {
+      const tickets = data.map(d => {
+        return {
+          id: d.ticket.id,
+          price: d.ticket.price
+        } as OrderTicketDTO;
+      })
+
+      console.log(user);
+      
+  
+      this.orderService.createOrder({
+        tickets: tickets,
+        username: user.sub //FIXME: this should be handled by token processing
+      }).subscribe({
+        next: () => {
+          this.router.navigate(['/cart']);
+        },
+        error: (e) => {
+          console.error(e);
+        }
+      });
+    })  
+    
+
+  }
+
+  private getOrderData(): OrderData[] {
+    const orderData = Array.from(this.ticketStore.getSnapshot().values());
+  
+    if (orderData.length === 0) {
+      throw new Error('No order data found. The array is empty.');
+    }
+    
+    console.log(orderData);
+    return orderData;
   }
 }
